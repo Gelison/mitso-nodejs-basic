6edbf47a85f58c0a15667c949ef400a4c4ebd51d
Gelison <daniil.sol7777@gmail.com>
2023-03-21 09:41:42 +0300

    start work

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,11 @@
 const copy = async () => {
-    // Write your code here 
+  // Write your code here
+  fs.mkdir('files_copy', (err) => {
+    if (err) throw err; // не удалось создать папку
+    console.log('Папка успешно создана');
+  });
+//   fs.copyFileSync('./files_copy', '');
+//   console.log('source.txt was copied to destination.txt');
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,15 @@
+let fs = requaire('fs');
 const create = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    fs.readFile('./files/fresh.txt', 'w+');
+  } catch (err) {
+    console.error('FS operation failed');
+  }
+  fs.writeFail('./files/fresh.txt', 'I am fresh and young', function (error) {
+    if (error) throw error;
+    console.log('write error');
+  });
 };
 
 await create();
